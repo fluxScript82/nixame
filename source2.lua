72a793ef655752289bbd04b09e7bad4d5df8d8e3
fluxScript82 <manangankhoby@gmail.com>
2025-08-20 21:38:42 +0800

    source2.lua

--- source2.lua
+++ source2.lua
@@ -4,7 +4,7 @@
     Author: Professional UI Team
     
     Usage:
-    local ProfessionalUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/fluxScript82/nixame/main/source.lua"))()
+    local ProfessionalUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/fluxScript82/nixame/refs/heads/main/source2.lua"))()
 ]]
 
 local ProfessionalUI = {}
